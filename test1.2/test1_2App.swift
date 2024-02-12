e87365dff1a2752d852dc5477c883252603554c2
miatroiano <m11ssing1n4ction@gmail.com>
2024-02-11 21:41:23 -0500

    first

---
+++ test1.2/test1_2App.swift
@@ -0,0 +1,17 @@
+//
+//  test1_2App.swift
+//  test1.2
+//
+//  Created by Mia Troiano on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct test1_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
